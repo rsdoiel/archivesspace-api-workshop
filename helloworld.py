62d0f70b630b6812bc21220b808f53fd80dbcfaa
R. S. Doiel <rsdoiel@gmail.com>
2016-07-26 15:45:59 -0700

    draft

--- helloworld.py
+++ helloworld.py
@@ -6,14 +6,14 @@ import json
 
 # This is where we add our functions
 def hello_world(name, secret):
-    print('I know you', name, ' and you have a secret...', secret)
+    print(name, 'has a secret...', secret)
     return 'Hello World!!!'
 
 # This is where we add our tests
 if __name__ == '__main__':
     import getpass
-    name = input('How are you? ')
-    secret = getpass.getpass('Tell me a secret I can pass on: ')
+    name = input('Who are you? ')
+    secret = getpass.getpass('Tell me a secret I can disclose...')
     hw = hello_world(name, secret)
     if hw == 'Hello World!!!':
         print('Success!! ', hw)
